63b55c6f8d7724898498244f0a3899b5652e2e86
emanmoq <42995029+emanmoq@users.noreply.github.com>
2022-11-28 17:43:04 -0800

    finish

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,38 @@
 jQuery(function ($) {
+    $(".datepicker").datepicker({
 
+    });
+    $('.select').selectric({
+        disableOnMobile: false,
+        nativeOnMobile: false,
+        arrowButtonMarkup: '<b class="button"><i class="fa fa-chevron-down"></i></b>',
+    }
+    );
+    $(".guestCar").owlCarousel({
+        nav: false,
+        dots: true,
+        loop: true,
+        margin: 30,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            700: {
+                items: 2,
+            },
+            1000: {
+                items: 3,
+            }
+        }
+    });
+    $(".serviceCar").owlCarousel({
+        nav: true,
+        dots: true,
+        loop: true,
+        margin:0,
+        items:1,
+        navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
 
-
+    });
 });
 
